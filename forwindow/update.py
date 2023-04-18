331474307a0fb04b922a47fe4a9dc8aec2cfb6e5
Felipe Pereira <70305395+Flepsz@users.noreply.github.com>
2023-04-18 01:42:24 +0000

    Update update.py

--- forwindow/update.py
+++ forwindow/update.py
@@ -5,9 +5,8 @@ cursor = conexao.cursor()
 
 def update_user():
     email = input("Digite o e-mail com permissão para atualizar o usuário: ")
-    sql = "SELECT tipo FROM usuarios WHERE email = %s"
-    val = (email,)
-    cursor.execute(sql, val)
+    sql = f"SELECT tipo FROM usuarios WHERE email = {email}"
+    cursor.execute(sql)
     result = cursor.fetchone()
 
     if result is None:
@@ -18,10 +17,8 @@ def update_user():
         nome = input("Digite o novo nome do usuário: ")
         idade = int(input("Digite a nova idade do usuário: "))
         plano = input("Digite o novo plano do usuário (Basic, Medium ou Premium): ").title()
-
-        sql = "UPDATE usuarios SET usuario = %s, idade = %s, plano = %s WHERE email = %s"
-        val = (nome, idade, plano, email)
-        cursor.execute(sql, val)
+        sql = f"UPDATE usuarios SET usuario = {nome}, idade = {idade}, plano = {plano} WHERE email = {email}"
+        cursor.execute(sql)
         conexao.commit()
 
         print("Usuário atualizado com sucesso")
@@ -29,9 +26,8 @@ def update_user():
 
 def update_film():
     email = input("Digite o e-mail do usuário com permissão para atualizar filmes: ")
-    sql = "SELECT tipo FROM usuarios WHERE email = %s"
-    val = (email,)
-    cursor.execute(sql, val)
+    sql = f"SELECT tipo FROM usuarios WHERE email = {email}"
+    cursor.execute(sql)
     result = cursor.fetchone()
 
     if result is None:
@@ -43,10 +39,8 @@ def update_film():
         filme = input("Digite o novo título do filme: ")
         plano = input("Digite o novo plano do usuário (Basic, Medium ou Premium): ").title()
         desc = input("Digite uma nova descrição para o filme: ")
-
-        sql = "UPDATE filmes SET filme = %s, plano = %s, ano = %s, desc = %s WHERE id_filme = %s"
-        val = (filme, plano, desc, id_filme)
-        cursor.execute(sql, val)
+        sql = f"UPDATE filmes SET filme = {filme}, plano = {plano}, desc = {desc} WHERE id_filme = {id_filme}"
+        cursor.execute(sql)
         conexao.commit()
 
         print("Filme atualizado com sucesso")
